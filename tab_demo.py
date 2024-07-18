a171d62754ced0527372a4d9a17bd061cb373ce1
Kratosssssss <46520478+Kratosssssss@users.noreply.github.com>
2024-07-18 14:57:55 +0800

    Update tab_demo.py

--- tab_demo.py
+++ tab_demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.image("image/banner.png", use_column_width=True)
-tab1, tab2, tab3, tab4 = st.tabs(["1.Upload Feedback Data", " 2️⃣ Preprocessing", " Step 3: Intelligent Scoring", "Step 4. Manage Knowledge Base"])
+tab1, tab2, tab3, tab4 = st.tabs(["1️⃣ Upload Feedback Data", " 2️⃣ Preprocessing", " 3️⃣ Intelligent Scoring", "4️⃣ Manage Knowledge Base"])
 
 
 prompt_textbox_css = """
@@ -90,37 +90,55 @@ with tab3:
     # Create columns to organize content
     left_column, middle_column, right_column = st.columns(3)
 
+    scoring_css = """
+    <style>
+    NumberInput label,
+    Slider label {
+        font-size: 20px !important;  /* Adjust the font size as needed */
+    }
+    </style>
+"""
+
+    # Inject the custom CSS into the app
+    st.markdown(scoring_css, unsafe_allow_html=True)
+
     # Sentiment Section
     with left_column:
         st.markdown("<h2 style='font-size: 24px;'>1. Sentiment</h2>", unsafe_allow_html=True)
-        sentiment_weight = st.number_input("Weight", 0, 100, 50, key="1")
+        st.markdown("<h2 style='font-size: 20px;'>Weight</h2>", unsafe_allow_html=True)
+        sentiment_weight = st.number_input("Weight", 0, 100, 50, key="1", label_visibility="collapsed")
 
         sentiment_options = {"Strong Negative": -2, "Negative": -1, "Neutral": 0, "Positive": 1, "Strong Positive": 2}
         sentiment_values = {}
         for sentiment, value in sentiment_options.items():
-            sentiment_values[sentiment] = st.slider(sentiment, -3, 3, value)
+            st.markdown(f"<h2 style='font-size: 20px;'>{sentiment}</h2>", unsafe_allow_html=True)
+            sentiment_values[sentiment] = st.slider(sentiment, 1, 5, value+3, label_visibility="collapsed")
 
     # Intent Section
     with middle_column:
         st.markdown("<h2 style='font-size: 24px;'>2. Intent</h2>", unsafe_allow_html=True)
-        intent_weight = st.number_input("Weight", 0, 100, 50, key="2")
+        st.markdown("<h2 style='font-size: 20px;'>Weight</h2>", unsafe_allow_html=True)
+        sentiment_weight = st.number_input("Weight", 0, 100, 50, key="3", label_visibility="collapsed")
 
         intent_options = {"Suggestion": 5, "Criticism": 4, "Warning": 3, "Observation": 2, "Complaint": 1}
         intent_values = {}
         for intent, value in intent_options.items():
-            intent_values[intent] = st.slider(intent, -3, 3, value)
+            st.markdown(f"<h2 style='font-size: 20px;'>{intent}</h2>", unsafe_allow_html=True)
+            sentiment_values[intent] = st.slider(intent, 1, 5, value, label_visibility="collapsed")
 
     # Topic Section
     with right_column:
         st.markdown("<h2 style='font-size: 24px;'>3. Topic</h2>", unsafe_allow_html=True)
-        topic_weight = st.number_input("Weight", 0, 100, 50, key="3")
+        st.markdown("<h2 style='font-size: 20px;'>Weight</h2>", unsafe_allow_html=True)
+        sentiment_weight = st.number_input("Weight", 0, 100, 50, key="2", label_visibility="collapsed")
 
         topic_options = {"Budget": 4, "Communication": 4, "Decision Making": 4, "Efficiency": 2, "Automation": 1,
                          "AI": 5,
                          "Change": 3, "Others": 1}
         topic_values = {}
         for topic, value in topic_options.items():
-            topic_values[topic] = st.slider(topic, -3, 3, value)
+            st.markdown(f"<h2 style='font-size: 20px;'>{topic}</h2>", unsafe_allow_html=True)
+            sentiment_values[topic] = st.slider(topic, 1, 10, value, label_visibility="collapsed")
 
     # Save Button
     _, right_button_col = st.columns([14, 1])
@@ -128,4 +146,4 @@ with tab3:
         st.button("Save", key="index=3")
 
 with tab4:
-    st.header('Intelligent Feedback Analyzer', divider='blue')
+    st.subheader('Intelligent Feedback Analyzer', divider='blue')
